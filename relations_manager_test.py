19eadc9aaf69bd603011f1e1d01c800a8eae7ebf
Rigi99 <64904615+Rigi99@users.noreply.github.com>
2023-03-01 18:34:00 +0200

    second test written
    Check if John Doe’s team members are Myrta Torkelson and Jettie Lynch.

--- relations_manager_test.py
+++ relations_manager_test.py
@@ -9,3 +9,16 @@ def test_john_doe_is_leader():
     john = Employee(id=1, first_name="John", last_name="Doe", base_salary=3000,
                     birth_date=date(1970, 1, 31), hire_date=date(1990, 10, 1))
     assert rm.is_leader(john) == True
+
+
+def test_john_doe_team_members():
+    rm = RelationsManager()
+    john = Employee(id=1, first_name="John", last_name="Doe", base_salary=3000,
+                    birth_date=date(1970, 1, 31), hire_date=date(1990, 10, 1))
+    team_members = rm.get_team_members(john)
+    all_employees = rm.get_all_employees()
+    assert len(team_members) == 2
+    assert any(emp.first_name == "Myrta" and emp.last_name == "Torkelson" and emp.id in team_members for emp in
+               all_employees)
+    assert any(emp.first_name == "Jettie" and emp.last_name == "Lynch" and emp.id in team_members for emp in
+               all_employees)
