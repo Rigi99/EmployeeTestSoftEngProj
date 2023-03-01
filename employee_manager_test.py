880db43127c926b787410f5f4ec2ab4c9009078c
Rigi99 <64904615+Rigi99@users.noreply.github.com>
2023-03-01 19:40:30 +0200

    second test written
    Adjustments in employee manager, to have 3 people in the team. Check an employee’s salary who is a team leader and his team consists of 3 members. She was hired on 10.10.2008 and has a base salary of 2000$. Validate if the returned value is 4100$ (2000$ + 15 X 100$ + 3 X 200$).

--- employee_manager_test.py
+++ employee_manager_test.py
@@ -11,3 +11,11 @@ def test_calculate_salary_for_non_leader_employee():
                         birth_date=datetime.date(1972, 10, 14), hire_date=datetime.date(1998, 10, 10))
     actual_salary = em.calculate_salary(employee)
     assert actual_salary == 3500
+
+
+def test_team_leader_salary_calculation():
+    rm = RelationsManager()
+    e1 = Employee(id=4, first_name="Gretchen", last_name="Watford", base_salary=2000,
+                  birth_date=datetime.date(1985, 8, 22), hire_date=datetime.date(2008, 10, 10))
+    em = EmployeeManager(rm)
+    assert em.calculate_salary(e1) == 4100
